237260518fff1d9d9b8f5ad8471951c7b478e569
Zpoetri01 <162399144+Zpoetri01@users.noreply.github.com>
2026-04-06 15:03:20 +0700

    Delete 2311102127_ZahraTsuroyyaPoetri directory

--- 2311102127_ZahraTsuroyyaPoetri/toko_kelontong/routes/web.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\ProdukController;
-
-Route::get('/', [ProdukController::class, 'index']);
-Route::post('/tambah', [ProdukController::class, 'tambah']);
-Route::post('/edit', [ProdukController::class, 'edit']);
-Route::post('/hapus', [ProdukController::class, 'hapus']);
